7af20ab753317b3ae9cc9caec1efd9de12140e32
Francesco Girlanda <66215169+FraGirla@users.noreply.github.com>
2023-06-20 15:17:29 +0200

    Script for the euler sbatch

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -n 10
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=5000
+#SBATCH --tmp=4000                        # per node!!
+#SBATCH --job-name=roberta_script
+#SBATCH --output=roberta_script.out
+#SBATCH --gpus=a100_80gb:1
+
+module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14.9 libsndfile/1.0.23
+pip install -r requirements.txt
+python roBERTa_regression_script.py
