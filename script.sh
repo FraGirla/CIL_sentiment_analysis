5e4f4413041a5efb68334d18137613f955f620ba
Francesco Girlanda <66215169+FraGirla@users.noreply.github.com>
2023-06-21 12:07:17 +0200

    Optimized script

--- script.sh
+++ script.sh
@@ -6,7 +6,8 @@
 #SBATCH --tmp=4000                        # per node!!
 #SBATCH --job-name=roberta_script
 #SBATCH --output=roberta_script.out
-#SBATCH --gpus=a100_80gb:1
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:29g
 
 module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14.9 libsndfile/1.0.23
 pip install -r requirements.txt
