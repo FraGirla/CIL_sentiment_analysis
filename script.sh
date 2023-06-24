a22bde92c3d3afca73d5ce0f6bff6c3378242a5f
Francesco Girlanda <66215169+FraGirla@users.noreply.github.com>
2023-06-24 18:59:23 +0200

    Higher time and high gpu mem

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH -n 10
-#SBATCH --time=48:00:00
+#SBATCH --time=96:00:00
 #SBATCH --mem-per-cpu=5000
 #SBATCH --tmp=4000                        # per node!!
 #SBATCH --job-name=roberta_script
 #SBATCH --output=roberta_script.out
 #SBATCH --gpus=1
-#SBATCH --gres=gpumem:29g
+#SBATCH --gres=gpumem:39g
 
 module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14.9 libsndfile/1.0.23
 pip install -r requirements.txt
