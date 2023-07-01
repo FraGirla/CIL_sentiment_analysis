a37088a4d5158cf0753f6307473dc9a8a4c80443
Matteo Mazzonelli <m.mazzonelli@gmail.com>
2023-07-01 18:32:33 +0200

    script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -n 10
-#SBATCH --time=96:00:00
+#SBATCH --time=360:00:00
 #SBATCH --mem-per-cpu=5000
 #SBATCH --tmp=4000                        # per node!!
 #SBATCH --job-name=bert_grid
