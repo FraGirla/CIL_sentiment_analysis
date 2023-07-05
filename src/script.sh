1609b0398a2da91a9de79a5c636fb1f5e909d194
Francesco Girlanda <66215169+FraGirla@users.noreply.github.com>
2023-07-05 14:38:30 +0200

    Moved into src

--- src/script.sh
+++ src/script.sh
@@ -4,11 +4,12 @@
 #SBATCH --time=360:00:00
 #SBATCH --mem-per-cpu=5000
 #SBATCH --tmp=4000                        # per node!!
-#SBATCH --job-name=bert_grid
-#SBATCH --output=bert_grid_search.out
+#SBATCH --job-name=train_script
+#SBATCH --output=train_script.out
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:41g
 
 module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14.9 libsndfile/1.0.23
 pip install -r requirements.txt
-python BERT_grid_search.py
+python train_script.py
+mv train_script.out train_script.out.out
