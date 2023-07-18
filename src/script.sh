cafed59e4419b1c34fa4140f22fc3fe2145406f3
Francesco Girlanda <66215169+FraGirla@users.noreply.github.com>
2023-07-18 17:21:34 +0200

    Fix module load

--- src/script.sh
+++ src/script.sh
@@ -9,7 +9,7 @@
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:41g
 
-module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14.9 libsndfile/1.0.23 wandb/0.12.17
+module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14.9 libsndfile/1.0.23
 pip install -r requirements.txt
 python train_script.py
 mv train_script.out train_script.out.out
